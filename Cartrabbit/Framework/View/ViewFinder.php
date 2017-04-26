dbbf4beed83b0ecbe77297e6366bbca42620dc98
Ashlin <ashlin.flycart@gmail.com>
2017-04-26 12:52:51 +0530

    - support namespace view

--- Cartrabbit/Framework/View/ViewFinder.php
+++ Cartrabbit/Framework/View/ViewFinder.php
@@ -2,10 +2,17 @@
 
 namespace Cartrabbit\Framework\View;
 
+use Cartrabbit\Framework\Application;
 use Illuminate\View\FileViewFinder;
+use InvalidArgumentException;
 
 class ViewFinder extends FileViewFinder
 {
+    public function __construct()
+    {
+        $app = Application::getInstance();
+        $this->app = $app;
+    }
     /**
      * Return a list of found views.
      *
@@ -27,10 +34,35 @@ class ViewFinder extends FileViewFinder
         if (isset($this->views[$name])) {
             return $this->views[$name];
         }
-
         if ($this->hasHintInformation($name = trim($name))) {
+            $segments = explode(static::HINT_PATH_DELIMITER, $name);
+            if(isset($this->app['path.'.$segments[0]])){
+                $this->hints[$segments[0]] = $this->app['path.'.$segments[0]];
+            }
             return $this->views[$name] = $this->findNamespacedView($name);
         }
         return $this->views[$name] = $this->findInPaths($name, $this->paths);
+    }
+
+    /**
+     * Find the given view in the list of paths.
+     *
+     * @param  string  $name
+     * @param  array   $paths
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function findInPaths($name, $paths)
+    {
+        foreach ((array) $paths as $path) {
+            foreach ($this->getPossibleViewFiles($name) as $file) {
+                if (file_exists($viewPath = $path.'/'.$file)) {
+                    return $viewPath;
+                }
+            }
+        }
+
+        throw new InvalidArgumentException("View [$name] not found.");
     }
 }
